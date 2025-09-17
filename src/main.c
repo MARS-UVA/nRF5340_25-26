733a799262b200cc1f338c2fd57aa4a2d3097198
Diana Lin <lindiana1206@gmail.com>
2025-09-17 17:23:03 -0400

    add bno055 via i2c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,64 @@
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/i2c.h>
+#include "bno055.h"
+
+#define I2C1_NODE DT_NODELABEL(imu)
+
+static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C1_NODE);
+
+void write_register(const struct i2c_dt_spec *spec, uint8_t reg, uint8_t value)
+{
+        uint8_t buf[2] = {reg, value};
+        int ret = i2c_write_dt(spec, buf, sizeof(buf));
+        if (ret)
+        {
+                printk("Failed to write to device: %d\n", ret);
+        }
+}
 
 int main(void)
 {
+        printk("Hello World! %s\n", CONFIG_BOARD);
+
+        if (!device_is_ready(dev_i2c.bus))
+        {
+                printk("I2C bus %s is not ready!\n\r", dev_i2c.bus->name);
+                return 0;
+        }
+
+        write_register(&dev_i2c, BNO055_OPR_MODE_ADDR, BNO055_OPERATION_MODE_CONFIG);
+
+        /* Reset */
+        // write_register(&dev_i2c, BNO055_SYS_TRIGGER_ADDR, 0x20);
+
+        write_register(&dev_i2c, BNO055_PWR_MODE_ADDR, BNO055_POWER_MODE_NORMAL);
+        k_msleep(10);
+
+        write_register(&dev_i2c, BNO055_PAGE_ID_ADDR, 0);
+
+        write_register(&dev_i2c, BNO055_SYS_TRIGGER_ADDR, 0x0);
+        k_msleep(10);
+
+        write_register(&dev_i2c, BNO055_OPR_MODE_ADDR, BNO055_OPERATION_MODE_NDOF);
+        k_msleep(500);
+
+        printk("IMU configured\n");
+
+        // read
+        while (1)
+        {
+                int16_t data;
+                int ret = i2c_burst_read_dt(&dev_i2c, BNO055_ACCEL_DATA_X_LSB_ADDR, (uint8_t *)&data, sizeof(data));
+                if (ret)
+                {
+                        printk("Failed to read from IMU: %d\n", ret);
+                        return 0;
+                }
+
+                printk("IMU data: %d\n", data);
+
+                k_sleep(K_MSEC(100));
+        }
+
         return 0;
 }
