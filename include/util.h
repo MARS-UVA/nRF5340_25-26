7770aa5cb5513c420a4bbe2d98a457a170929dc1
aedan <118068380+AedanStewart@users.noreply.github.com>
2025-11-02 15:38:07 -0500

    add max, min, map util functions

--- include/util.h
+++ include/util.h
@@ -3,5 +3,9 @@
 
 void float_to_byte_array(float f, char *arr);
 void float_16_to_byte_array(float f, char *arr);
+int min(int a, int b);
+int max(int a, int b);
+float map(int min, int max, int pos);
+
 
 #endif /* INC_UTIL_H_ */
